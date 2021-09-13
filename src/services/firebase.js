63fe2566540d3321c0d57c547e47dfbf59aaf760
samx23 <skalexsong@gmail.com>
2021-09-13 22:28:36 +0700

    Update : add firebase services

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1Myqx6wan3mRvEDeLYpMVsxcE5ROekcg",
+  authDomain: "binar-full-stack-web-academy.firebaseapp.com",
+  projectId: "binar-full-stack-web-academy",
+  storageBucket: "binar-full-stack-web-academy.appspot.com",
+  messagingSenderId: "47775109673",
+  appId: "1:47775109673:web:c9e9636cb51349ee2ac238",
+  measurementId: "G-3E7H6LPM6F",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
