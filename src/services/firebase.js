7a05597ce8776c83577d519831b74fbc4e93e9f9
samx23 <skalexsong@gmail.com>
2021-09-15 20:33:04 +0700

    Update : firebase js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1Myqx6wan3mRvEDeLYpMVsxcE5ROekcg",
-  authDomain: "binar-full-stack-web-academy.firebaseapp.com",
-  projectId: "binar-full-stack-web-academy",
-  storageBucket: "binar-full-stack-web-academy.appspot.com",
-  messagingSenderId: "47775109673",
-  appId: "1:47775109673:web:c9e9636cb51349ee2ac238",
-  measurementId: "G-3E7H6LPM6F",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
